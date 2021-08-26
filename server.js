ec97d92cb22a6c25b55f3e14dbc098b33d84dc34
Natalie Agus <nn88blue@gmail.com>
2021-08-26 17:59:43 +0800

    style: Better welcome message in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 8000;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end("My first server!");
+    res.end("My first server! Hello world!");
 };
 
 const server = http.createServer(requestListener);
